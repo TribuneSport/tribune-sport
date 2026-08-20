d61e95c45d911e2d84d69903af152ca871ad7cd2
TribuneSport <cfmy2123@gmail.com>
2026-08-20 17:16:13 +0200

    Fix autorisation des images RSS

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -27,6 +27,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "io-fsly-bfmtv.cdn.nextradiotv.com",
       },
+      {
+        protocol: "https",
+        hostname: "image.lessentiel.lu",
+      },
     ],
   },
 };
